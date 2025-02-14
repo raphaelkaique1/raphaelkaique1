b4a8e00a4528a3774aaae0dbabc024211f2de7fc
raphaelkaique1 <raphaelkaiquediassantos1@gmail.com>
2025-02-14 13:46:54 -0300

    feat: create RESUME

---
+++ main/js/script.js
@@ -0,0 +1,5 @@
+document.getElementById("xp").textContent = xp;
+document.getElementById("year").textContent = currentYear;
+const yearStart = 2020;
+const currentYear = new Date().getFullYear();
+const xp = currentYear - yearStart;
