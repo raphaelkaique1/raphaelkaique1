80ab1708bf8d77c05499c2affe0e5db41ed4dcb9
raphaelkaique1 <raphaelkaiquediassantos1@gmail.com>
2025-02-15 07:27:02 -0300

    fix: add meqdia query back-button position image_galery

--- main/js/script.js
+++ main/js/script.js
@@ -4,5 +4,5 @@ const yearStart = 2020;
 const currentYear = new Date().getFullYear();
 const xp = currentYear - yearStart;
 
-document.getElementById("xp").textContent = xp;
 document.getElementById("year").textContent = currentYear;
+document.getElementById("xp").textContent = xp;
