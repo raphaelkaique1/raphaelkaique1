6de99496e77c430e49b703e65ae0b364ab4e83c8
raphaelkaique1 <raphaelkaiquediassantos1@gmail.com>
2025-02-19 16:57:23 -0300

    feat: deploy teach_me

--- projects/teach_me/vite.config.ts
+++ projects/teach_me/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/raphaelkaique1/'
+  base: '/raphaelkaique1/projects/teach_me/',
 })
