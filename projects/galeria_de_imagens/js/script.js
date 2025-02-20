b2da6de8905f70f17c229964f27bf7051a055dbb
raphaelkaique1 <raphaelkaiquediassantos1@gmail.com>
2025-02-20 07:19:21 -0300

    feat: update projects dir

--- projects/galeria_de_imagens/js/script.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const theme = document.getElementById("theme");
-theme.addEventListener("click", function() {
-    document.body.classList.toggle("dark-theme");
-    let className = document.body.className;
-    className == "light-theme" ? this.textContent = "switch dark theme" : this.textContent = "switch light theme";
-});
--- projects/teach_me/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/raphaelkaique1/projects/teach_me/dist/',
-})
