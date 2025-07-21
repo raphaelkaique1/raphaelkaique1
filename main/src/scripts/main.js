dbd64ef75a242468049c95ddad9908ebfa52d981
raphaelkaique1 <raphaelkaiquediassantos1@gmail.com>
2025-07-21 09:48:05 -0300

    ⚡️ Create preloader function.

--- main/src/scripts/main.js
+++ main/src/scripts/main.js
@@ -1,2 +1,10 @@
 const currentYear = new Date().getFullYear();
 document.getElementById("year").textContent = currentYear;
+
+window.addEventListener("load", function() {
+  const preloader = document.getElementById("preloader");
+  const content = document.getElementById("content");
+
+  preloader.style.display = "none";
+  content.style.display = "block";
+});
